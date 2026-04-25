74e75e23d6498ed833b9e28c304e95125ac424ca
Olugbemi Tolulope <olugbemiopedepo@gmail.com>
2026-04-25 15:45:58 +0100

    Implement Litestore storefront architecture (app/create/page.tsx)

---
+++ app/create/page.tsx
@@ -0,0 +1,9 @@
+import { CreateStudio } from '@/components/litestore/create-studio';
+
+export const metadata = {
+  title: 'Create store'
+};
+
+export default function CreatePage() {
+  return <CreateStudio />;
+}
